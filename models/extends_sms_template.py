e27655ae865344e3092aab152aee0a3dcadf73d0
levislibra <levislibra@hotmail.com>
2023-11-22 11:21:20 -0300

    Update

--- models/extends_sms_template.py
+++ models/extends_sms_template.py
@@ -7,5 +7,13 @@ class ExtendsSmsTemplate(models.Model):
 	_inherit = 'sms.template'
 	_description = 'Plantilla de SMS'
 
+	body_len = fields.Integer('Cantidad de caracteres', compute='_compute_body_len')
+	body_len_text = fields.Char('Cantidad de caracteres en texto', compute='_compute_body_len')
 	company_id = fields.Many2one('res.company', string='Empresa', index=True, default=lambda self: self.env.company)
 
+	@api.onchange('body')
+	def _compute_body_len(self):
+		if self.body:
+			body_len = len(self.body)
+			self.body_len_text = str(body_len) + " caracteres de 160 como maximo."
+			self.body_len = body_len
